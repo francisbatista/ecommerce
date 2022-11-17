944bf7514618fbd1413f3003925cc2298d43cf36
Francis Batista <tipofrancis@gmail.com>
2022-11-17 17:23:38 -0400

    agregando mas funcionalidad

---
+++ scripts/general.js
@@ -0,0 +1,8 @@
+const inputsMensajeNombre = document.querySelector('#iNombreMensaje');
+const inputsMensaje = document.querySelector('#iMensaje');
+
+inputsMensaje.setAttribute('required', 'true');
+inputsMensaje.setAttribute('maxlength', '120');
+
+inputsMensajeNombre.setAttribute('required', 'true');
+inputsMensajeNombre.setAttribute('maxlebgth', '40');
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 const btnLogin = document.querySelector('.cabecera__btn-login');
+const btnVerConsolas = document.querySelector('#btnVerConsolas');
+
 btnLogin.addEventListener('click',()=>{
-    window.open('login.html');
-    console.log("hola");
+    window.open('login.html', target='_parent');
+});
+
+btnVerConsolas.addEventListener('click', () => {
+    location.href = '#consolas';
 });
